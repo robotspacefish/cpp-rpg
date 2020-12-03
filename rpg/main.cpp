54b849051415d9754909bee76ca8e57f7fbad44c
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-12-03 12:40:26 -0500

    Initial Commit

---
+++ rpg/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  rpg
+//
+//  Created by Jess on 12/3/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
