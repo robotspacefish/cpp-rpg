ca9a054f10a95c506c2e5714b4bc66c12440315e
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-12-04 21:53:09 -0500

    generate a random enemy type

--- rpg/main.cpp
+++ rpg/main.cpp
@@ -6,10 +6,56 @@
 //
 
 #include <iostream>
+#include <string>
+#include <ctime>
+
 enum characterType { PLAYER, OGRE, GOBLIN, SLIME, SKELLY };
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int randomEnemyType() {
+    return rand() % 4 + 1;
+}
+
+std::string getEnemyTypeAsString(int type)
+{
+    switch (type) {
+        case OGRE:
+            return "Ogre";
+        case GOBLIN:
+            return "Goblin";
+        case SLIME:
+            return "Slime";
+        case SKELLY:
+            return "Skelly";
+        default:
+            return "Invalid Enemy Type";
+    }
+}
+
+int main() {
+    srand(time_t(NULL));
+    
+    enum moveType { ATTACK = 1, RUN };
+    std::string enemyName;
+    std::cout << enemyName;
+    int input = 0;
+    
+    while (input != 2) {
+        std::cout << "1. Generate Enemy\n";
+        std::cout << "2. Quit\n\n";
+        
+        std::cout << "Enter a selection: ";
+        std::cin >> input;
+        
+        switch (input) {
+            case 1:
+                std::cout << "Enemy Type: " << getEnemyTypeAsString(randomEnemyType()) << "\n";
+                break;
+            case 2:
+                std::cout << "Goodbye!";
+            default:
+                std::cout << "Invalid selection";
+                break;
+        }
+    }
     return 0;
 }
